b93f141176427077a90f1d073743d2ba318f72d0
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-06-30 13:31:28 +0200

    added vars

--- vm-joined-to-active-directory/variables.tf
+++ vm-joined-to-active-directory/variables.tf
@@ -1,16 +1,20 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "vm0292dd"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "eastus"
 }
 
 variable "admin_username" {
   description = "Username for the Administrator account"
+  default = "labadmin"
 }
 
 variable "admin_password" {
   description = "Password for the Administrator account"
+  default = "ArcDemo123!!"
 }
 
