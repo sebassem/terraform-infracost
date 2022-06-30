31eb8911035e64cba339f12c1013add2465d784f
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-06-30 13:35:19 +0200

    added location

--- vm-joined-to-active-directory/modules/domain-member/variables.tf
+++ vm-joined-to-active-directory/modules/domain-member/variables.tf
@@ -4,6 +4,7 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "eastus"
 }
 
 variable "resource_group_name" {
